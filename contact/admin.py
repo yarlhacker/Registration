ef9177bb21d9db50a5781c70b87a9d1db2de66dd
ecolenan <ismael.developper@gmail.com>
2021-06-16 10:16:21 +0000

    first

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . import models 
+
+
+@admin.register(models.Profil)
+class ProdilAdmin(admin.ModelAdmin):
+
+    list_display = ["user","prenom","photo","phone", "date_creat","date_update","status"]
+    list_editable = ('status',)
+
+
+
+@admin.register(models.Contact)
+class ProdilAdmin(admin.ModelAdmin):
+
+    list_display = ["user","prenom","photo","phone","nom","email", "date_creat","date_update","status"]
+    list_editable = ('status',)
+
+    
+
 
 # Register your models here.
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User 
+
+
+class Base(models.Model):
+
+    date_creat = models.DateField( auto_now_add=True)
+    date_update = models.DateField(auto_now=True)
+    status  = models.BooleanField(default=True)
+
+    class Meta : 
+        abstract = True
+
+
+class Profil(Base):
+    user = models.OneToOneField(User, verbose_name=("Userdjango"), on_delete=models.CASCADE)
+    prenom = models.CharField( max_length=250)
+    photo = models.FileField( upload_to='Images')
+    phone = models.CharField( max_length=250)
+
+
+    def __str__(self):
+        return self.prenom
+    
+    class Meta:
+        verbose_name = 'Profil'
+        verbose_name_plural = 'Profils'
+
+
+class Contact(Base):
+
+    user = models.ForeignKey(User, verbose_name=("Profil"), on_delete=models.CASCADE)
+    photo = models.FileField( upload_to='Images')
+    nom = models.CharField( max_length=250)
+    prenom = models.CharField( max_length=250)
+    email = models.EmailField(max_length=254)
+    phone = models.CharField( max_length=250)
+
+
+    def __str__(self):
+        return self.nom
+    
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+
 
 # Create your models here.
